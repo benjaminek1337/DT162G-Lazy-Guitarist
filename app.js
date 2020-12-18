84a28e187d8e8e1b40ee329cb92a778845cd099c
Benjamin Ek <benjaminek@live.se>
2020-12-18 09:16:48 +0100

    asd

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.set('trust proxy', 1)
 // }));
 
 app.all("/*", (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://lazyguitarist.herokuapp.com/");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
