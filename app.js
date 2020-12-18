655f7162d0b4e1b964e3d7ddb9397ddb51a2dbb8
Benjamin Ek <benjaminek@live.se>
2020-12-18 10:03:43 +0100

    changed route

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.all("/*", (req, res, next) => {
 // }); 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + 'public/index.html'));
 });
 
 
