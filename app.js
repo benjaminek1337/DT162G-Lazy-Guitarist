84a4d567f97b7a8dbe50496542c25b6a5a455212
Benjamin Ek <benjaminek@live.se>
2020-12-18 10:24:00 +0100

    asd

--- app.js
+++ app.js
@@ -79,10 +79,10 @@ app.use("/api/spotify", spotifyRouter);
 app.use("/api/user", userRouter);
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static('client/build'));
+    app.use(express.static('public'));
     app.get('*', (req, res) => {
         res.sendFile(
-            path.resolve(__dirname, 'client', 'build', 'index.html')
+            path.resolve(__dirname, 'public', 'index.html')
         );
     });
 }
